3f4dcff994d36f435e4e11e7990dd021f00d9316
Joe Manthey <jdmanthey@gmail.com>
2025-06-22 11:17:41 -0500

    Update 00_index_ref.sh

--- 00_index_ref.sh
+++ 00_index_ref.sh
@@ -4,8 +4,8 @@ cd references
 
 source activate bcftools
 
-samtools faidx GCF_003259725.1_athCun1_genomic.fna
+samtools faidx GCA_003259725.1_athCun1_genomic.fna
 
 java -jar picard.jar CreateSequenceDictionary \
-R=/home/jmanthey/references/GCF_003259725.1_athCun1_genomic.fna \
-O=/home/jmanthey/references/GCF_003259725.1_athCun1_genomic.dict
+R=/home/jmanthey/references/GCA_003259725.1_athCun1_genomic.fna \
+O=/home/jmanthey/references/GCA_003259725.1_athCun1_genomic.dict
