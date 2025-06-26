99082268e6c7189483cef3c40079a202c3a7320f
Joe Manthey <jdmanthey@gmail.com>
2025-06-26 10:41:34 -0500

    Update 06_consensus_tree.sh

--- 06_consensus_tree.sh
+++ 06_consensus_tree.sh
@@ -1,1 +1,1 @@
-sumtrees.py --output=_glauc_50kbp_consensus.tre --min-clade-freq=0.01 _glauc_50kbp_windows.trees
+sumtrees.py --output=_glauc_50kbp_consensus.tre --min-clade-freq=0.01 _glauc_50kbp_windows_renamed.trees
