ddb4b6186d4dc215e520d0b4e835e87b887ec03a
Joe Manthey <jdmanthey@gmail.com>
2025-07-07 11:13:35 -0500

    Update 07_consensus_tree.sh

--- 07_consensus_tree.sh
+++ 07_consensus_tree.sh
@@ -1,1 +1,5 @@
+# maximum clade credibility tree
 sumtrees.py --output=_glauc_50kbp_consensus.tre --min-clade-freq=0.01 _glauc_50kbp_windows_renamed.trees
+
+# ASTRAL 
+java -jar ~/Astral/astral.5.6.3.jar -i_glauc_50kbp_windows_renamed.trees -o glauc_50kbp_astral.tre 2> glauc_50kbp_astral.log
